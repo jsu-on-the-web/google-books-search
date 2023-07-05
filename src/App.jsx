9b698c07c054025ddb855943055a63d71416bed6
Janet Suryadi <janet@suryadi.id.au>
2023-07-05 11:28:51 +1000

    Fixing Header import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from './Header';
+import { Header } from './components/Header/Header';
 import { useState, useEffect } from 'react'
 import './App.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
