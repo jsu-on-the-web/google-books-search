3ed60bb9e4ad5c7d2344a61c51af0ac23e9a9ea9
Janet Suryadi <janet@suryadi.id.au>
2023-07-09 23:03:13 +1000

    Making the useTransitionOnSpawn hook

---
+++ src/hooks/useTransitionOnSpawn.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+/*----------  Function Declaration  ----------*/
+
+/**
+ * A hook that manages the transition state of a component when iy spawns in.
+ *
+ * @param {boolean} isSpawned - indicates whether the component has spawned or not
+ * @param {number} despawnDelay - the delay in milliseconds before the component is despawned
+ * @return {boolean} true if the component has transitioned in, false otherwise
+ */
+const useSpawnTransition = (isSpawned, despawnDelay) => {
+    const [hasTransitionedIn, setHasTransitionedIn] = useState(false);
+
+    // Making use of useEffect for the actual logic of the hook
+    useEffect(() => {
+        let timeoutId;
+
+        if (isSpawned) {
+            setHasTransitionedIn(true);
+        } else if (!isSpawned && hasTransitionedIn) { 
+            // Currently despawning/unmounting.
+            timeoutId = setTimeout(() => {
+                setHasTransitionedIn(false);
+            }, despawnDelay);
+        }
+
+        return () => {
+            // Get rid of the timeout
+            clearTimeout(timeoutId);
+        };
+    }, [despawnDelay, isSpawned, hasTransitionedIn]);
+
+    return hasTransitionedIn;
+}
+
+export default useSpawnTransition;
