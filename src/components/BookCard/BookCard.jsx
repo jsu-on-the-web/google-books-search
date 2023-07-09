7666d3e0f9c950b90fd39f09654bbeff3cbf20f0
Janet Suryadi <janet@suryadi.id.au>
2023-07-09 19:15:14 +1000

    Styling

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './BookCard.module.scss'
+import styles from './BookCard.module.scss'
 
 export function BookCard({ coverThumbnailSrc = "https://picsum.photos/200", title, authors, description }) {
   return (
-    <div className='results-card'>
+    <div className={styles["results-card"]}>
       <img src={coverThumbnailSrc} className='results-card__image' />
       <h3 className='results-card__title'>{title}</h3>
       <h4 className='results-card__author'>{authors}</h4>
