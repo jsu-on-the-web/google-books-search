6313acb002a06669d4aab8cf2994edb8a003a6c8
Janet Suryadi <janet@suryadi.id.au>
2023-07-10 22:25:02 +1000

    Responsive design?

--- src/components/BookModal/BookModal.jsx
+++ src/components/BookModal/BookModal.jsx
@@ -25,17 +25,18 @@ export function BookModal({ book, onClose }) {
         return null;
     }
     return (
-        <div className={styles['book-modal']}>
-            <img src={book.thumbnail} alt="" className='book-modal__image' />
-            <h3 className='book-modal__title'>{book.title}</h3>
-            <h4 className='book-modal__author'>{bookAuthors}</h4>
-            <h4 className='book-modal__publisher'>{book.publisher}</h4>
-            <h4 className='book-modal__published-date'>{book.publishedDate}</h4>
-            <p className='book-modal__average-rating'><Rating value={book.averageRating} precision={0.5} readOnly /></p>
-            <p className='book-modal__description'>{bookDescription}</p>
-            {/* <p className='book-modal__language'>  Language: <img src={getImage(book.language)}></img></p> */} 
-
-        </div>
+        <section className={styles["book-modal-container"]}>
+            <div className={styles['book-modal']}>
+                <img src={book.thumbnail} alt="" className='book-modal__image' />
+                <h3 className='book-modal__title'>{book.title}</h3>
+                <h4 className='book-modal__author'>{bookAuthors}</h4>
+                <h4 className='book-modal__publisher'>{book.publisher}</h4>
+                <h4 className='book-modal__published-date'>{book.publishedDate}</h4>
+                <p className='book-modal__average-rating'><Rating value={book.averageRating} precision={0.5} readOnly /></p>
+                <p className='book-modal__description'>{bookDescription}</p>
+                {/* <p className='book-modal__language'>  Language: <img src={getImage(book.language)}></img></p> */}
+            </div>
+        </section>
     );
 }
 
